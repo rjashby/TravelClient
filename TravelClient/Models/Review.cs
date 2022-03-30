523a6a01d222cd112297f3852398397fd6d029bf
Ryan Ashby <rjapsu160@gmail.com>
2022-03-30 09:58:32 -0700

    Ensure API and MVC routes connect successfully
    Co-authored-by: DrewHendersonGitHub <DrewHendersonGitHub@users.noreply.github.com>

--- TravelClient/Models/Review.cs
+++ TravelClient/Models/Review.cs
@@ -8,6 +8,7 @@ namespace TravelClient.Models
   public class Review
   {
     public int ReviewId { get; set; }
+    public int DestinationId { get; set; }
     public string Title { get; set; }
     public string Content { get; set; }
     public double Rating { get; set; }
