4f1458f4d2e4b2fefde63524edaa4cfdf7588d3e
Ryan Ashby <rjapsu160@gmail.com>
2022-03-30 09:04:54 -0700

    re-draft client... alllll of it
    Co-authored-by: DrewHendersonGitHub <DrewHendersonGitHub@users.noreply.github.com>

---
+++ TravelClient/Models/Review.cs
@@ -0,0 +1,54 @@
+using System.Collections.Generic;
+using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace TravelClient.Models
+{
+  public class Review
+  {
+    public int ReviewId { get; set; }
+    public string Title { get; set; }
+    public string Content { get; set; }
+    public double Rating { get; set; }
+
+    public static List<Review> GetReviews()
+    {
+      var apiCallTask = ApiHelper.GetAll();
+      var result = apiCallTask.Result;
+
+      JArray jsonResponse = JsonConvert.DeserializeObject<JArray>(result);
+      List<Review> reviewList = JsonConvert.DeserializeObject<List<Review>>(jsonResponse.ToString());
+
+      return reviewList;
+    }
+
+    public static Review GetDetails(int id)
+    {
+      var apiCallTask = ApiHelper.Get(id);
+      var result = apiCallTask.Result;
+
+      JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(result);
+      Review review = JsonConvert.DeserializeObject<Review>(jsonResponse.ToString());
+
+      return review;
+    }
+
+    public static void Post(Review review)
+    {
+      string jsonReview = JsonConvert.SerializeObject(review);
+      var apiCallTask = ApiHelper.Post(jsonReview);
+    }
+
+    public static void Put(Review review)
+    {
+      string jsonReview = JsonConvert.SerializeObject(review);
+      var apiCallTask = ApiHelper.Put(review.ReviewId, jsonReview);
+    }
+
+    public static void Delete(int id)
+    {
+      var apiCallTask = ApiHelper.Delete(id);
+    }
+  }
+}
